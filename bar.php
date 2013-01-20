2f664c65aaad134f113bd4918221383721cbdf10
Chad Ackerman <chad.ackerman@nitelifecam.com>
2013-01-20 12:29:05 -0500

    added sections
    added other sections of the bar page with their flags

--- bar.php
+++ bar.php
@@ -9,7 +9,7 @@
 
 
 	<div class="content">
-		<div class="specials-section">
+		<div class="section">
 			<img src="images/specials-flag.png" />
 			<div class="specials-date-picker">
 				<table class="date-picker" border="0" cellspacing="0" cellpadding="0">
@@ -37,4 +37,46 @@
 				</table>
 			</div>
 		</div>
+
+		<div class="section">
+			<img src="images/hh-flag.png" />
+			<div class="specials-date-picker">
+				<table class="date-picker" border="0" cellspacing="0" cellpadding="0">
+					<tr>
+						<td class="active-date">M</td>
+						<td>T</td>
+						<td>W</td>
+						<td>R</td>
+						<td>F</td>
+						<td>S</td>
+						<td>U</td>
+					</tr>
+				</table>
+			</div>
+			<div class="hh-viewer">
+				<table class="specials-table" border="0" cellspacing="0" cellpadding="0">
+					<tr>
+						<td><div class="active-special">1/2 off appetizers</div></td>
+						<td><div class="special-time">4pm-6pm</div></td>
+					</tr>
+					<tr>
+						<td><div class="active-special">$1 wells</div></td>
+						<td><div class="special-time">7pm-9pm</div></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+
+		<div class="section">
+			<img src="images/info-flag.png" />
+			<div class="info-section">
+				<span>Sunday</span> 11am-5pm<br>
+				<span>Mon-Wed</span> 11am-5pm<br>
+				<span>Fri-Sat</span> 11am-5pm<br>
+			</div>
+		</div>
+
 	</div>
+
+
+
