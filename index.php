40723883da40ec2b8c6fa4828a7c26a8448f48ad
Britney Bogard <fazzel09@gmail.com>
2013-02-06 15:39:43 -0500

    removed some comments

--- index.php
+++ index.php
@@ -48,30 +48,6 @@ require('scripts/database.php');
 
 	</script>
 
-	// <script>
-	// function loadXMLDoc()
-	// {
-	// var xmlhttp;
-	// if (window.XMLHttpRequest)
-	//   {// code for IE7+, Firefox, Chrome, Opera, Safari
-	//   xmlhttp=new XMLHttpRequest();
-	//   }
-	// else
-	//   {// code for IE6, IE5
-	//   xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	//   }
-	// xmlhttp.onreadystatechange=function()
-	//   {
-	//   if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	//     {
-	//     document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-	//     }
-	//   }
-	// xmlhttp.open("GET","demo_get.asp?t=" + Math.random(),true);
-	// xmlhttp.send();
-	// }
-	// </script>
-
 </head>
 
 
